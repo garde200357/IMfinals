f2c2dd13a9611a9ecebadd2a9654aafb79bceee3
garde200357 <169014892+garde200357@users.noreply.github.com>
2024-11-15 15:13:31 +0800

    Add files via upload

---
+++ libisgym_db (1).sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 15, 2024 at 08:09 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `libisgym_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_book`
+--
+
+CREATE TABLE `tbl_book` (
+  `book_id` int(100) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `bdate` date NOT NULL,
+  `btime` time NOT NULL,
+  `remarks` varchar(255) NOT NULL,
+  `book_stats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_message`
+--
+
+CREATE TABLE `tbl_message` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_status`
+--
+
+CREATE TABLE `tbl_status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_status`
+--
+
+INSERT INTO `tbl_status` (`id`, `status`) VALUES
+(1, 'Accepted'),
+(2, 'Rejected'),
+(3, 'Pending'),
+(4, 'Finished');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(100) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `birthdate` date NOT NULL,
+  `gender` enum('Male','Female','Other') NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pword` varchar(20) NOT NULL,
+  `user_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `firstname`, `lastname`, `phone`, `birthdate`, `gender`, `email`, `pword`, `user_level`) VALUES
+(1, 'Libis', 'Gym', '09507472357', '2017-01-01', 'Other', 'libisgym@gmail.com', 'Admin#123', 1),
+(17, 'Jeremy', 'Sabino', '09454665775', '2002-10-25', 'Male', 'jeremysabino@gmail.com', 'Jeremy#123', 2),
+(18, 'Dan Axel', 'Torren', '09523656895', '2002-05-30', 'Male', 'danaxeltorren@gmail.com', 'Danaxel#123', 2),
+(19, 'John Paolo', 'Salazar', '09463577807', '2002-11-27', 'Male', 'johnpaolosalazar@gmail.com', 'Johnpaolo#123', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_userlevel`
+--
+
+CREATE TABLE `tbl_userlevel` (
+  `id` int(11) NOT NULL,
+  `user_level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_userlevel`
+--
+
+INSERT INTO `tbl_userlevel` (`id`, `user_level`) VALUES
+(1, 'Admin'),
+(2, 'Guest'),
+(3, 'Premium Guest');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_book`
+--
+ALTER TABLE `tbl_book`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `book_stats` (`book_stats`);
+
+--
+-- Indexes for table `tbl_message`
+--
+ALTER TABLE `tbl_message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_status`
+--
+ALTER TABLE `tbl_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_level` (`user_level`);
+
+--
+-- Indexes for table `tbl_userlevel`
+--
+ALTER TABLE `tbl_userlevel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_book`
+--
+ALTER TABLE `tbl_book`
+  MODIFY `book_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_message`
+--
+ALTER TABLE `tbl_message`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_status`
+--
+ALTER TABLE `tbl_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tbl_userlevel`
+--
+ALTER TABLE `tbl_userlevel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_book`
+--
+ALTER TABLE `tbl_book`
+  ADD CONSTRAINT `tbl_book_ibfk_1` FOREIGN KEY (`book_stats`) REFERENCES `tbl_status` (`id`);
+
+--
+-- Constraints for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD CONSTRAINT `tbl_user_ibfk_1` FOREIGN KEY (`user_level`) REFERENCES `tbl_userlevel` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
